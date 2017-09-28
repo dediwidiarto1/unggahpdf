6bfd1a07acab2a4c357632d3b91156bda93edf42
dediwidiarto1 <widiartodedi@gmail.com>
2017-09-28 16:58:45 +0700

    Add files via upload

---
+++ tambahan/menubar.php
@@ -0,0 +1,62 @@
+<div class="col-md-3 left_col">
+  <div class="left_col scroll-view">
+    <div class="navbar nav_title" style="border: 0;">
+      <a href="index.php" class="site_title"><i class="fa fa-desktop"></i> <span>STBI UU</span></a>
+    </div>
+    <div class="clearfix"></div>
+    
+    <!-- /menu profile quick info -->
+    <br />
+    <!-- sidebar menu -->
+    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+      <div class="menu_section">
+        <ul class="nav side-menu">
+              <li><a href="index.php?link=pencariandokumen"><i class="fa fa-search"></i> Pencarian Dokumen</a>
+  </li>
+          <li><a href="index.php?link=stbi"><i class="fa fa-book"></i> STBI</a>
+        </li>
+        <li><a><i class="fa fa-table"></i> Data Master <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="index.php?link=datadokumen">Data Dokumen</a></li>
+          <li><a href="index.php?link=datastoplist">Data Stoplist</a></li>
+          <li><a href="index.php?link=datakatadasar">Data Kata Dasar</a></li>
+        </ul>
+      </li>
+      <li><a><i class="fa fa-puzzle-piece"></i> Proses STBI <span class="fa fa-chevron-down"></span></a>
+      <ul class="nav child_menu">
+        <li><a href="index.php?link=hapustandabaca"> Hapus Tanda Baca</a></li>
+        <li><a href="index.php?link=lowercase"> Lower Case Dokumen</a></li>
+        <li><a href="index.php?link=parsing"> Tokenisasi</a></li>
+        <li><a href="index.php?link=hasilindex"> Indexing</a></li>
+        <li><a href="index.php?link=hasilstopword">Stopword</a></li>
+        <li><a href="index.php?link=hasilstemming">Stemming</a></li>
+        <li><a href="index.php?link=documentfreq">Document Frequensi</a></li>
+        <li><a href="index.php?link=inversdocfreq">Invers Document Frequensi</a></li>
+        <li><a href="index.php?link=tf_x_idf">Weighting</a></li>
+        <li><a href="index.php?link=rweight">Retrive Weight</a></li>
+      </ul>
+    </li>
+  <li><a href="index.php?link=about"><i class="fa fa-info-circle"></i> Tentang</a>
+  <li><a href="index.php?link=resetdata" onclick="return confirm('Reset Data akan menghapus semua data ! Lanjutkan ?')"><i class="fa fa-exclamation-triangle"></i> Reset Data</a>
+</li>
+</ul>
+</div>
+
+</div>
+<!-- /sidebar menu -->
+</div>
+</div>
+<!-- top navigation -->
+<div class="top_nav">
+<div class="nav_menu">
+<nav>
+<div class="nav toggle">
+<a id="menu_toggle"><i class="fa fa-bars"></i></a>
+</div>
+<ul class="nav navbar-nav navbar-right">
+<h2>STBI Undang-Undang</h2>
+</ul>
+</nav>
+</div>
+</div>
+<!-- /top navigation -->
